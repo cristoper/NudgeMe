e778054eb1937c8165c66b184056b2e73ff861cb
A. Cynic <chris@mretc.net>
2018-01-31 11:37:54 -0700

    Ignore exception and add note on error handling

--- ItemArray.swift
+++ ItemArray.swift
@@ -11,9 +11,14 @@ struct ItemArray {
     
     // This is a stored property (whose type is an Array of Strings) which will store the
     // list of items in memory once they are read from the plist file
-    var items: [String] = []
+    var items: [String] = ["Go for a jog", "Work on your novel", "Read 20 pages in your current book"]
     
     init (plistName name: String = "items") {
+        /* A note on error handling in this initializer -
+           We use everywhere explicit unwrapping of optionals (!), non-propagating exceptions (try!) and forced downcasting (as!). The result is that any error in getting the main bundle, reading the plist.info file, or converting it to a [string] variable will crash the program.
+
+           Since we can be reasonably sure that none of thes will fail (we provide the info.plist along with the application), but a more robust program would allow any of these to fail, log/report the error, then use a reasonable default value for items.
+        */
         
         // Build a path to the plist file (assuming it is in the main bundle,
         // which is where Xcode puts things by default
@@ -23,22 +28,15 @@ struct ItemArray {
         // Read the file into a variable.
         // the return type of the FileManager contents() method is an NSData object
         // which is basically a bunch of bytes
-        let data = FileManager.default.contents(atPath: path)
+        let data = FileManager.default.contents(atPath: path)!
         
-        // the propertyList() method throws an exception when it encounters an error,
-        // so we wrap it in a do-catch construct
-        do {
-            // The propertyList method converts our data into an Array
-            // we cast it to an array of strings using the 'as' type casting operator:
-            // https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TypeCasting.html
-            try items = PropertyListSerialization.propertyList(
-                from: data!,
-                options:PropertyListSerialization.MutabilityOptions.mutableContainers,
-                format: nil) as! [String]
-        } catch let e as NSError {
-            // If there is an error, print its description to the concole
-            print(e.localizedDescription)
-        }
+        // The propertyList method converts our data into an Array
+        // we cast it to an array of strings using the 'as' type casting operator:
+        // https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TypeCasting.html
+        items = try! PropertyListSerialization.propertyList(
+            from: data,
+            options:PropertyListSerialization.MutabilityOptions.mutableContainers,
+            format: nil) as! [String]
     }
     
     func randomItem() -> String {
